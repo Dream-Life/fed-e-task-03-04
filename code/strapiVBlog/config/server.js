8fabd322ee34afdd7b42eef84d896e2b22d9c709
Ting.ye <ting.ye@MacBook-Pro.local>
2021-01-05 10:18:25 +0800

    save code

---
+++ code/strapiVBlog/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a45adf49f2f432f66e1be31157d231bb'),
+    },
+  },
+});
---
+++ code/strapiVBlog/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cdf3f871-0dc0-4aa5-a71b-9f2c763e8afe'
+};
